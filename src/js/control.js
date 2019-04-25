567053230fa617f8163eb5a826e65459d481d16e
TailyFair <Karolis.Kalzanauskas@stud.vgtu.lt>
2019-04-25 21:59:16 +0300

    Forward and Backward controls added

--- src/js/control.js
+++ src/js/control.js
@@ -1,5 +1,7 @@
 var player = document.getElementById("example-video");
 
+const timeSpan = 5;
+
 function play() {
     player.play();
 }
@@ -11,4 +13,12 @@ function pause() {
 function stop() {
     player.pause();
     player.currentTime(0);
+}
+
+function forward() {
+    player.currentTime(player.currentTime() + timeSpan);
+}
+
+function backward() {
+    player.currentTime(player.currentTime() - timeSpan);
 }
